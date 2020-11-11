52254bd5371a76cf2d778bbb451f9a8a4372d42f
nikoo <nicoloscialpi@gmail.com>
2020-11-11 17:02:06 +0100

    Calibrating teleport distance #4

--- src/main/java/it/areson/interdimension/events/PlayerPassPortalEvents.java
+++ src/main/java/it/areson/interdimension/events/PlayerPassPortalEvents.java
@@ -40,7 +40,8 @@ public class PlayerPassPortalEvents implements Listener {
                 if (toLocation.getChunk().equals(portal.getPortalChunk())) {
                     Location to = toLocation;
                     if (to.distance(portal.getLocation()) < .5 ||
-                            e.getPlayer().getEyeLocation().clone().distance(portal.getLocation()) < .5
+                            e.getPlayer().getEyeLocation().clone().distance(portal.getLocation()) < .5 ||
+                            to.distance(portal.getLocation().clone().subtract(0,1,0)) < .5
                     ) {
                         portal.teleport(e.getPlayer());
                         portal.spark();
