09a0c23cf755f3bfd925f936b87c35dd70246118
nikoo <nicoloscialpi@gmail.com>
2020-11-11 16:22:59 +0100

    Fix location finder

--- src/main/java/it/areson/interdimension/utils/PortalLocationFinder.java
+++ src/main/java/it/areson/interdimension/utils/PortalLocationFinder.java
@@ -3,25 +3,35 @@ package it.areson.interdimension.utils;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 public class PortalLocationFinder {
 
-    public static Location findOptimalLocationForPortal(Player player){
+    public static Location findOptimalLocationForPortal(Player player) {
         Location playerLocation = player.getLocation().clone();
-        Location behind = playerLocation.clone().subtract(playerLocation.getDirection().multiply(2));
+        Vector direction = playerLocation.getDirection();
+        direction = direction.multiply(new Vector(1, 0, 1));
+        direction = direction.normalize();
+        Location behind = playerLocation.clone().subtract(direction.multiply(2));
         Location testLocation = behind.clone();
-        while(testLocation.getBlock().getType().equals(Material.AIR) &&
-                testLocation.clone().subtract(0,1,0).getBlock().getType().equals(Material.AIR) &&
-                testLocation.getY()<=200){
-            testLocation.add(0,3,0);
+        while (!isPortalSpaceAir(testLocation) &&
+                testLocation.getY() <= 200) {
+            testLocation.add(0, 1, 0);
         }
-        if(testLocation.getY()>=200){
+        if (testLocation.getY() >= 200) {
             testLocation = behind.clone();
-            while(testLocation.getBlock().getType().equals(Material.AIR) && testLocation.getY()>=5){
-                testLocation.subtract(0,2,0);
+            while (!isPortalSpaceAir(testLocation) && testLocation.getY() >= 5) {
+                testLocation.subtract(0, 1, 0);
             }
         }
         return testLocation;
+    }
+
+    public static boolean isPortalSpaceAir(Location location) {
+        return location.getBlock().getType().equals(Material.AIR) &&
+                location.clone().subtract(0, 1, 0).getBlock().getType().equals(Material.AIR) &&
+                location.clone().add(0, 1, 0).getBlock().getType().equals(Material.AIR);
+
     }
 
 }
