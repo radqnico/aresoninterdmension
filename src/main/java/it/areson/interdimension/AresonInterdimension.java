11ac11f13e8abd7d6b29b66c52895e49849ac3c2
Marco Paggioro <marco.paggioro@gmail.com>
2021-02-13 17:41:07 +0100

    Useless commit

--- src/main/java/it/areson/interdimension/AresonInterdimension.java
+++ src/main/java/it/areson/interdimension/AresonInterdimension.java
@@ -10,6 +10,7 @@ import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AresonInterdimension extends JavaPlugin {
+    //TODO Useless commit
 
     public MessageManager messages;
     public FileManager data;
